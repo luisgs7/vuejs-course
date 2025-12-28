1617269b8fd9cf548c8fb22cce6440b98f66a856
luisgs7 <luisgsilva270@gmail.com>
2025-12-28 16:39:03 -0300

    Add job listing and detail views; implement navigation buttons and 404 handling

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,6 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
+import Jobs from '@/views/jobs/jobs.vue'
+import JobDetail from '@/views/jobs/jobDetail.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {
@@ -15,11 +18,32 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: AboutView  //() => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetail',
+    component: JobDetail
+  },
+  // Redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // Catchall 404
+  {
+    path: '/:catchAll(.*)*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ ninja-jobs/src/views/jobs/jobs.vue
@@ -0,0 +1,49 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
+            <h2>{{ job.title }}</h2>
+        </router-link>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'Jobs',
+        data() {
+            return {
+                jobs: [
+                    { id: 1, title: 'Web Developer', location: 'New York', salary: 100000 },
+                    { id: 2, title: 'Frontend Developer', location: 'San Francisco', salary: 120000 },
+                    { id: 3, title: 'Backend Developer', location: 'Chicago', salary: 110000 },
+                    { id: 4, title: 'Full Stack Developer', location: 'Boston', salary: 130000 },
+                    { id: 5, title: 'Software Engineer', location: 'Los Angeles', salary: 140000 },
+                    { id: 6, title: 'Database Administrator', location: 'Seattle', salary: 150000 },
+                    { id: 7, title: 'Network Engineer', location: 'Miami', salary: 160000 },
+                    { id: 8, title: 'System Administrator', location: 'Austin', salary: 170000 },
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+.job h2 {
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+}
+
+.job h2:hover {
+    background: #ddd;
+}
+
+.job a {
+    text-decoration: none;
+}
+</style>
