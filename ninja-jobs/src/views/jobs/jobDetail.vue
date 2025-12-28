27d20e191cdf10d2c1503fb4075d1ca5bd5ddce3
luisgs7 <luisgsilva270@gmail.com>
2025-12-28 17:26:08 -0300

    Add JSON server dependency, create mock job data, and update job listing and detail views to fetch data from the server.

--- ninja-jobs/src/views/jobs/jobDetail.vue
+++ ninja-jobs/src/views/jobs/jobDetail.vue
@@ -1,6 +1,12 @@
 <template>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p>The job id is {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+        <h2>Loading job...</h2>
+    </div>
 </template>
 
 <script>
@@ -8,7 +14,14 @@
         data() {
             return {
                 id: this.$route.params.id,
+                job: null
             }
+        },
+        mounted() {
+            fetch(`http://localhost:3000/jobs/${this.id}`)
+            .then(response => response.json())
+            .then(data => this.job = data)
+            .catch(error => console.log(error))
         }
     }
 </script>
--- ninja-jobs/src/views/jobs/jobs.vue
+++ ninja-jobs/src/views/jobs/jobs.vue
@@ -1,11 +1,15 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-        <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
-            <h2>{{ job.title }}</h2>
-        </router-link>
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
+                <h2>{{ job.title }}</h2>
+            </router-link>
+        </div>
     </div>
-
+    <div v-else>
+        <h2>Loading jobs...</h2>
+    </div>
 </template>
 
 <script>
@@ -13,17 +17,14 @@
         name: 'Jobs',
         data() {
             return {
-                jobs: [
-                    { id: 1, title: 'Web Developer', location: 'New York', salary: 100000 },
-                    { id: 2, title: 'Frontend Developer', location: 'San Francisco', salary: 120000 },
-                    { id: 3, title: 'Backend Developer', location: 'Chicago', salary: 110000 },
-                    { id: 4, title: 'Full Stack Developer', location: 'Boston', salary: 130000 },
-                    { id: 5, title: 'Software Engineer', location: 'Los Angeles', salary: 140000 },
-                    { id: 6, title: 'Database Administrator', location: 'Seattle', salary: 150000 },
-                    { id: 7, title: 'Network Engineer', location: 'Miami', salary: 160000 },
-                    { id: 8, title: 'System Administrator', location: 'Austin', salary: 170000 },
-                ]
+                jobs: []
             }
+        },
+        mounted() {
+            fetch('http://localhost:3000/jobs')
+            .then(response => response.json())
+            .then(data => this.jobs = data)
+            .catch(error => console.log(error))
         }
     }
 </script>
