5f29f14b067a7a8cf2945df2f5aee3d6a919a3d0
luisgs7 <luisgsilva270@gmail.com>
2026-01-06 21:54:37 -0300

    Add post details view and implement post fetching logic; enhance SinglePost component with router link and tags display.

--- dojo-blog/src/router/index.js
+++ dojo-blog/src/router/index.js
@@ -1,16 +1,23 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Details from '../views/Details.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -12,29 +12,14 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import PostList from '../components/PostList.vue';
+import getPosts from '../composables/getPosts';
 
 export default {
   name: 'Home',
   components: {PostList},
   setup() {
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try{
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok) {
-          throw Error('No data available')
-        }
-        posts.value = await data.json()
-      } catch(err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
-
+    const { posts, error, load } = getPosts()
     load()
 
     return { posts, error }
